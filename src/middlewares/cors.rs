487dd2101f390f3a33d698eb7de0a13f01126dcc
Yasir Shariff <goodwonder5@gmail.com>
2022-02-27 07:49:39 +0300

    updated cors

--- src/middlewares/cors.rs
+++ src/middlewares/cors.rs
@@ -1,12 +1,12 @@
 // Cors Middleware
 
-use tower_http::cors::{any, CorsLayer};
+use tower_http::cors::{any, Any, CorsLayer};
 
 pub fn cors() -> CorsLayer {
     let cors = CorsLayer::new()
-        .allow_methods(any())
-        .allow_origin(any())
+        .allow_methods(Any)
+        .allow_origin(Any)
         .allow_credentials(true)
-        .allow_headers(any());
+        .allow_headers(Any);
     cors
 }
